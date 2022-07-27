a225904aa098b6f43d24f6b69abfedf9036f82dc
Zook1234 <59753693+NotQuiteZook@users.noreply.github.com>
2022-07-27 14:40:16 -0400

    fix

--- index.mjs
+++ index.mjs
@@ -1,21 +1,28 @@
-import Server from 'bare-server-node';
+import createBareServer from '@tomphttp/bare-server-node';
 import http from 'http';
 import nodeStatic from 'node-static';
-
-
-const bare =  new Server('/bare/', '');
+const bare = createBareServer('/bare/', {
+    logErrors: true
+});
 const serve = new nodeStatic.Server('static/');
-
 const server = http.createServer();
 
 server.on('request', (request, response) => {
-    if (bare.route_request(request, response)) return true;
-    serve.serve(request, response);
+    if (bare.shouldRoute(request)) {
+        bare.routeRequest(request, response);
+    } else {
+        serve.serve(request, response);
+    }
 });
 
 server.on('upgrade', (req, socket, head) => {
-	if(bare.route_upgrade(req, socket, head))return;
-	socket.end();
+    if (bare.shouldRoute(req)) {
+        bare.routeUpgrade(req, socket, head);
+    } else {
+        socket.end();
+    }
 });
 
-server.listen(process.env.PORT || 8080);
+server.listen({
+    port: process.env.PORT || 8080,
+});
